1bd8604c79d0984be2c0aaa2dd0cce8169fb8b58
yuhaoyang123 <39150374+yuhaoyang123@users.noreply.github.com>
2019-09-21 10:55:45 +0800

    Update bubbleSort.py

--- leetcode/bubbleSort.py
+++ leetcode/bubbleSort.py
@@ -8,18 +8,6 @@ def Bubble_sort(x):
     return x
 print(Bubble_sort(x))
 
-#二分查找
-x = [1,3,4,6,7,8,9,10,11,12,13]
-find = 8
-def binary_chop(x,find):
-    while len(x) > 1:
-        if find > x[len(x)//2]:
-            x = x[len(x)//2:len(x)]
-        elif find < x[len(x)//2]:
-            x = x[0:len(x)//2]
-        else:
-            return x[len(x)//2]
-print(binary_chop(x,find))
-      
+
 
 
